7ebac07e7ead214e27ec49468abf651b9b9b65c3
Werner Gehrke Junior <werner.junior@outlook.com>
2021-01-18 20:29:20 -0300

    Layout base dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://www.portaldosanimais.com.br/wp-content/uploads/2019/05/Capivara-1-1.jpg"
+            alt="Capivara"
+          />
+          <div>
+            <strong>Capivara Repo</strong>
+            <p>
+              Capivara programadora Capivara programadora Capivara programadora
+              Capivara programadora Capivara programadora
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://www.portaldosanimais.com.br/wp-content/uploads/2019/05/Capivara-1-1.jpg"
+            alt="Capivara"
+          />
+          <div>
+            <strong>Capivara Repo</strong>
+            <p>
+              Capivara programadora Capivara programadora Capivara programadora
+              Capivara programadora Capivara programadora
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://www.portaldosanimais.com.br/wp-content/uploads/2019/05/Capivara-1-1.jpg"
+            alt="Capivara"
+          />
+          <div>
+            <strong>Capivara Repo</strong>
+            <p>
+              Capivara programadora Capivara programadora Capivara programadora
+              Capivara programadora Capivara programadora
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
