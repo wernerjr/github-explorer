e35c12539501bc18c6a08a1f8de3b279ff65b2f5
Werner Gehrke Junior <werner.junior@outlook.com>
2021-01-19 22:06:16 -0300

    Estilização pagina de repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -75,7 +76,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={Math.random()} href="teste">
+          <Link key={Math.random()} to={`/repository/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -85,7 +86,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://www.portaldosanimais.com.br/wp-content/uploads/2019/05/Capivara-1-1.jpg"
+            alt="capivara"
+          />
+          <div>
+            <strong>Teste/123/capivara</strong>
+            <p>teste capivara</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>12</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>32</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>54</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link key={Math.random()} to="13123123">
+          <div>
+            <strong>123123123</strong>
+            <p>123123123123</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
